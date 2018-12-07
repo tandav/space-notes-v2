7c06f6167eb57cba72c7b14ad9eda026d34d9992
Alexander Rodionov <alx1dr@gmail.com>
2018-12-07 20:06:03 +0300

    column view

--- src/App.js
+++ src/App.js
@@ -1,28 +1,64 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
 
 class App extends Component {
+  state = {
+    items: [
+      [
+        {name: 'space_1', type: 'folder'},
+        {name: 'space_2', type: 'folder'},
+        {name: 'file_1', type: 'file'},
+        {name: 'file_2', type: 'file'},
+        {name: 'file_3', type: 'file'},
+      ],
+      [
+        {name: 'space_3', type: 'folder'},
+        {name: 'space_4', type: 'folder'},
+        {name: 'file_54', type: 'file'},
+        {name: 'file_55', type: 'file'},
+        {name: 'file_56', type: 'file'},
+        {name: 'file_57', type: 'file'},
+        {name: 'file_58', type: 'file'},
+      ],
+      [
+        {name: 'space_45', type: 'folder'},
+        {name: 'space_46', type: 'folder'},
+        {name: 'file_104', type: 'file'},
+        {name: 'file_105', type: 'file'},
+      ],
+    ],
+    selected: ['space_2', 'space_4', 'file_105']
+  }
+
+  fake_api() {
+
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className='App'>
+        <div className='spaces'>
+          {
+            this.state.items.map((space, i) => {
+              return(
+                <ul className='space'>
+                  <h1>{i}</h1>
+                  {
+                    space.map(item => {
+                      if (item.name === this.state.selected[i]) {
+                        return <li>SELECTED {item.name}</li>
+                      }
+                      else
+                        return <li>{item.name}</li>
+                    })
+                  }
+                </ul>
+              )
+            })
+          }
+        </div>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
